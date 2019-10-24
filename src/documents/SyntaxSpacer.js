4c2119e55c1ac658bd5903067dcf3120a3b9d02e
august von trapp <augustvontrapp@gmail.com>
2019-10-24 02:42:39 -0700

    add ability to add sentence from the new inputs

--- src/documents/SyntaxSpacer.js
+++ src/documents/SyntaxSpacer.js
@@ -4,11 +4,22 @@ import SentenceInput from './SentenceInput'
 
 export default ({pointID, addItem, index}) => {
 
+  const addSentence = (val) => {
+    addItem({
+      locIndex: index,
+      type: 'sentences',
+      item: {text: val},
+      parentID: pointID,
+      parentType: 'points'
+    })
+  }
+
+
   const [clicked, setClicked ] = useState(false)
 
   return <span className={styles.syntaxSentenceSpacer}>
     {clicked ?
-      SentenceInput
+      <SentenceInput addItem={addSentence} mode={1} init={true} active={true}/>
       :
       <div onClick={() => setClicked(true)} className={styles.syntaxAdd}>+ {index}</div>
     }
